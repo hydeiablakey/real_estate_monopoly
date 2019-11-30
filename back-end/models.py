fdd69e6b83bd82952672908d1181ac83c575aa75
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-11-30 16:41:36 -0500

    create Properties model

--- back-end/models.py
+++ back-end/models.py
@@ -1,8 +1,7 @@
 
 from server import db
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
-
 
 
 class Users(db.Model):
@@ -20,56 +19,31 @@ class Users(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
-    
+
     def serialize(self):
         return {
-            'id': self.id, 
+            'id': self.id,
             'user_name': self.user_name,
             'email': self.email,
-            'password':self.password
+            'password': self.password
         }
 
-'''
-class Properties(db.Model):
-    __tablename__ = 'books'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    author = db.Column(db.String())
-    published = db.Column(db.String())
-
-    def __init__(self, name, author, published):
-        self.name = name
-        self.author = author
-        self.published = published
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
-    
-    def serialize(self):
-        return {
-            'id': self.id, 
-            'name': self.name,
-            'author': self.author,
-            'published':self.published
-        }
-'''
 
 class Games(db.Model):
     __tablename__ = 'games'
 
     id = db.Column(db.Integer, primary_key=True)
-    started = db.Column(db.Boolean, default=False)
+    started = db.Column(db.Boolean, default=False)  # new
 
     def __init__(self, started):
         self.started = started
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
-    
+
     def serialize(self):
         return {
-            'id': self.id, 
+            'id': self.id,
             'started': self.started
         }
 
@@ -77,8 +51,8 @@ class Games(db.Model):
 class Players(db.Model):
     __tablename__ = 'players'
 
-    user_id = db.Column(db.Integer, ForeignKey(Users.id), primary_key=True) 
-    game_id = db.Column(db.Integer, ForeignKey(Games.id), primary_key=True)  
+    user_id = db.Column(db.Integer, ForeignKey(Users.id), primary_key=True)
+    game_id = db.Column(db.Integer, ForeignKey(Games.id))
 
     money = db.Column(db.Integer)
     location = db.Column(db.String())
@@ -87,7 +61,7 @@ class Players(db.Model):
 
     users = relationship('Users', foreign_keys='Players.user_id')
     games = relationship('Games', foreign_keys='Players.game_id')
-   
+    
 
     def __init__(self, in_lobby, is_playing, money, location):
         self.in_lobby = in_lobby
@@ -97,13 +71,64 @@ class Players(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
-    
+
     def serialize(self):
         return {
-            'user_id': self.user_id, 
+            'user_id': self.user_id,
+            'player_id':self.player_id,
             'game_id': self.game_id,
             'money': self.money,
-            'location':self.location,
+            'location': self.location,
             'in_lobby': self.in_lobby,
             'is_playing': self.is_playing
+        }
+
+
+class Properties(db.Model):
+    __tablename__ = 'properties'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    address = db.Column(db.String())
+    p_type = db.Column(db.String())  # new
+    is_mortgaged = db.Column(db.Boolean, default=False)
+    mortgage_price = db.Column(db.Integer, default=0)
+    price = db.Column(db.Integer, default=0)
+    rent_price = db.Column(db.Integer, default=0)
+    upgrade_price = db.Column(db.Integer, default=0)
+    level = db.Column(db.Integer, default=0)  # new
+
+    owner_id = db.Column(db.Integer, ForeignKey(Players.user_id))
+    game_id = db.Column(db.Integer, ForeignKey(Games.id))
+
+    players = relationship('Players', foreign_keys='Properties.owner_id')
+    games = relationship('Games', foreign_keys='Properties.game_id')
+
+    def __init__(self, name, address, p_type, is_mortgaged, mortgage_price, price, rent_price, upgrade_price, level):
+        self.name = name
+        self.address = address
+        self.p_type = p_type
+        self.is_mortgaged = is_mortgaged
+        self.mortgage_price = mortgage_price
+        self.price = price
+        self.rent_price = rent_price
+        self.upgrade_price = upgrade_price
+        self.level = level
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'name': self.name,
+            'address': self.address,
+            'p_type': self.p_type,
+            'is_mortgaged': self.is_mortgaged,
+            'mortgage_price': self.mortgage_price,
+            'price': self.price,
+            'rent_price': self.rent_price,
+            'upgrade_price': self.upgrade_price,
+            'level': self.level,
+            'owner_id': self.owner_id,
+            'game_id': self.game_id
         }
