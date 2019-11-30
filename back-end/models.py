83a05080fd3751e8ca31cd66e339137d00857d1c
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-11-30 15:12:47 -0500

    create Players model

--- back-end/models.py
+++ back-end/models.py
@@ -1,5 +1,8 @@
 
 from server import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 
 
 class Users(db.Model):
@@ -50,30 +53,6 @@ class Properties(db.Model):
             'author': self.author,
             'published':self.published
         }
-
-class Players(db.Model):
-    __tablename__ = 'books'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    author = db.Column(db.String())
-    published = db.Column(db.String())
-
-    def __init__(self, name, author, published):
-        self.name = name
-        self.author = author
-        self.published = published
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
-    
-    def serialize(self):
-        return {
-            'id': self.id, 
-            'name': self.name,
-            'author': self.author,
-            'published':self.published
-        }
 '''
 
 class Games(db.Model):
@@ -92,4 +71,39 @@ class Games(db.Model):
         return {
             'id': self.id, 
             'started': self.started
+        }
+
+
+class Players(db.Model):
+    __tablename__ = 'players'
+
+    user_id = db.Column(db.Integer, ForeignKey(Users.id), primary_key=True) 
+    game_id = db.Column(db.Integer, ForeignKey(Games.id), primary_key=True)  
+
+    money = db.Column(db.Integer)
+    location = db.Column(db.String())
+    in_lobby = db.Column(db.Boolean, default=False)
+    is_playing = db.Column(db.Boolean, default=False)
+
+    users = relationship('Users', foreign_keys='Players.user_id')
+    games = relationship('Games', foreign_keys='Players.game_id')
+   
+
+    def __init__(self, in_lobby, is_playing, money, location):
+        self.in_lobby = in_lobby
+        self.is_playing = is_playing
+        self.money = money
+        self.location = location
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'user_id': self.user_id, 
+            'game_id': self.game_id,
+            'money': self.money,
+            'location':self.location,
+            'in_lobby': self.in_lobby,
+            'is_playing': self.is_playing
         }
